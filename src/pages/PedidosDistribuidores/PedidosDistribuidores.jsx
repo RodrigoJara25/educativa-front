60c33d627bbb1f5b1b83a35f3a1a008745f9c69f
RodrigoJara25 <r.jara.e7@gmail.com>
2026-02-23 15:42:20 -0500

    feature: Pedidos Diccionarios

--- src/pages/PedidosDistribuidores/PedidosDistribuidores.jsx
+++ src/pages/PedidosDistribuidores/PedidosDistribuidores.jsx
@@ -2,6 +2,7 @@ import "./PedidosDistribuidores.scss";
 import { useState } from "react";
 import PedidosDistribuidoresDatos from "../../components/PedidosDistribuidoresDatos/PedidosDistribuidoresDatos";
 import PedidosDistribuidoresCategorias from "../../components/PedidosDistribuidoresCategorias/PedidosDistribuidoresCategorias";
+import PedidosDiccionarios from "../../components/PedidosDiccionarios/PedidosDiccionarios";
 
 
 function PedidosDistribuidores() {
@@ -10,6 +11,7 @@ function PedidosDistribuidores() {
         <>
             {step === 1 && <PedidosDistribuidoresDatos onContinuar={() => setStep(2)} />}
             {step === 2 && <PedidosDistribuidoresCategorias onRetroceder={() => setStep(1)} onContinuar={() => setStep(3)} />}
+            {step === 3 && <PedidosDiccionarios onRetroceder={() => setStep(2)} onContinuar={() => setStep(4)} />}
         </>
     );
 }
